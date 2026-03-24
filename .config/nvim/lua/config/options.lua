91cb2cb92ff94ea78d577903df0b7400a970aacd
Leo <leo.hilsheimer@gmail.com>
2026-03-24 23:12:53 +0100

    show diagnostics not inline

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -2,3 +2,9 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 vim.opt.wrap = true
+
+-- show LSP errors, warnings, etc. in their own line
+vim.diagnostic.config({
+  virtual_lines = true,
+  virtual_text = false,
+})
