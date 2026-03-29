a1d9753222043f89d02222d74712c8ca9ae67ef3
Leo <leo.hilsheimer@gmail.com>
2026-03-29 17:40:53 +0200

    use prettierd in favor of prettier

--- .config/nvim/lua/plugins/formatting.lua
+++ .config/nvim/lua/plugins/formatting.lua
@@ -1,9 +1,12 @@
 return {
   "stevearc/conform.nvim",
-  opts = {
-    formatters_by_ft = {
-      json = { "prettier" },
-      jsonc = { "prettier" },
-    },
-  },
+  opts = function(_, opts)
+    opts.formatters_by_ft = vim.tbl_deep_extend("force", opts.formatters_by_ft or {}, {
+      json = { "prettierd" },
+      jsonc = { "prettierd" },
+      css = { "prettierd" },
+      scss = { "prettierd" },
+    })
+    return opts
+  end,
 }
