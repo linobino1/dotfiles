3f2d704ebc99abca148c103232b6002f6ffe6ddd
Leo <leo.hilsheimer@gmail.com>
2026-03-30 19:35:24 +0200

    use ai.sidekick + ai.copilot-native plugins

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -6,9 +6,6 @@ vim.keymap.set("i", "jj", "<Esc>")
 -- Exit terminal mode with Esc
 vim.keymap.set("t", "<Esc>", [[<C-\><C-n>]], { desc = "Exit terminal mode" })
 
--- Copilot Chat
-vim.keymap.set({ "n", "v" }, "<leader>cc", "<cmd>CopilotChat<CR>", { desc = "Copilot Chat" })
-
 vim.keymap.set("n", "<leader>rr", function()
   require("lazy").reload()
 end, { desc = "Reload LazyVim config" })
