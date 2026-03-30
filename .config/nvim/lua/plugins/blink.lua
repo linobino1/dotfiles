cb14713765a354ff8aaa43dea0020f4b85c6e4af
Leo <leo.hilsheimer@gmail.com>
2026-03-30 19:35:00 +0200

    turn off auto-parenthesis after accepting a completion

---
+++ .config/nvim/lua/plugins/blink.lua
@@ -0,0 +1,12 @@
+return {
+  "saghen/blink.cmp",
+  opts = {
+    completion = {
+      accept = {
+        auto_brackets = {
+          enabled = false, -- turn of auto-parenthesis after accepting a completion item
+        },
+      },
+    },
+  },
+}
